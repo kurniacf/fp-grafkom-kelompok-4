d90bce17f5bb7ef1139f03e48835dda2bc41e3e6
Muhammad Ismail <muhammadismail2418@gmail.com>
2022-12-14 13:23:51 +0700

    membenarkan codingan

--- js/game.js
+++ js/game.js
@@ -131,64 +131,6 @@ function resetGame(){
     }
   }
 }
-function resetGame() {
-  game = {
-    speed: 0,
-    initSpeed: 0.00035,
-    baseSpeed: 0.00035,
-    targetBaseSpeed: 0.00035,
-    incrementSpeedByTime: 0, //.0000025,
-    incrementSpeedByLevel: 0.000005,
-    distanceForSpeedUpdate: 100,
-    speedLastUpdate: 0,
-    score: 0,
-    distance: 0,
-    ratioSpeedDistance: 50,
-    energy: 100,
-    ratioSpeedEnergy: 3,
-    level: 1,
-    levelLastUpdate: 0,
-    distanceForLevelUpdate: 1000,
-    planeDefaultHeight: 100,
-    planeAmpHeight: 80,
-    planeAmpWidth: 75,
-    planeMoveSensivity: 0.005,
-    planeRotXSensivity: 0.0008,
-    planeRotZSensivity: 0.0004,
-    planeFallSpeed: 0.001,
-    planeMinSpeed: 1.2,
-    planeMaxSpeed: 1.6,
-    planeSpeed: 0,
-    planeCollisionDisplacementX: 0,
-    planeCollisionSpeedX: 0,
-    planeCollisionDisplacementY: 0,
-    planeCollisionSpeedY: 0,
-    waterRadius: 600,
-    waterLength: 800,
-    //seaRotationSpeed:0.006,
-    wavesMinAmp: 5,
-    wavesMaxAmp: 20,
-    wavesMinSpeed: 0.001,
-    wavesMaxSpeed: 0.003,
-    cameraFarPos: 500,
-    cameraNearPos: 150,
-    cameraSensivity: 0.002,
-    coinDistanceTolerance: 15,
-    coinValue: 10,
-    coinsSpeed: 0.5,
-    coinLastSpawn: 0,
-    distanceForCoinsSpawn: 100,
-    obstacleDistanceTolerance: 10,
-    obstacleValue: 10,
-    obstaclesSpeed: 0.6,
-    obstacleLastSpawn: 0,
-    distanceForObstaclesSpawn: 50,
-    status: "playing",
-  };
-  scoreCounter.innerHTML = "Score = " + game.score;
-  healthCounter.innerHTML = "Health = " + game.energy;
-}
-
 // Fungsi untuk menginisialisasi scene
 function createScene() {
   HEIGHT = window.innerHeight;
@@ -312,9 +254,9 @@ function loop() {
     if (is3D) {
       camera.lookAt(
         new THREE.Vector3(
-          airplane.mesh.position.x,
-          airplane.mesh.position.y,
-          airplane.mesh.position.z
+          airplane.rocketGroup.position.x,
+          airplane.rocketGroup.position.y,
+          airplane.rocketGroup.position.z
         )
       );
     }
